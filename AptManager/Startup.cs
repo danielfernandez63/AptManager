7126f4903d72e3cd8476aa4c310c2099d95a78a8
danielfernandez63 <danmanfernan@gmail.com>
2018-08-06 14:26:06 -0500

    added user roles

--- AptManager/Startup.cs
+++ AptManager/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using AptManager.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(AptManager.Startup))]
@@ -9,6 +12,69 @@ namespace AptManager
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesAndUsers();
         }
+
+
+
+        private void createRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!RoleManager.RoleExists("Admin"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                RoleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "BruceBanner";
+                user.Email = "danmanfernan@gmail.com";
+
+                string userPWD = "D3vC0d3$tud3nt";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                if (chkUser.Succeeded)
+                {
+                    var result = UserManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            if (!RoleManager.RoleExists("HousingManager"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Manager";
+                RoleManager.Create(role);
+            }
+
+
+            if (!RoleManager.RoleExists("Tenant"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Tenant";
+                RoleManager.Create(role);
+            }
+
+            if (!RoleManager.RoleExists("Maintenance"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Maintenance";
+                RoleManager.Create(role);
+            }
+
+
+            if (!RoleManager.RoleExists("Visitor"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Visitor";
+                RoleManager.Create(role);
+            }
+
+        }
+
     }
 }
