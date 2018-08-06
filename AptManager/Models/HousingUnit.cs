a97387d6602119ef2ac61bdbde14518238a06fea
phodgson558 <pghodgson558@gmail.com>
2018-08-06 11:53:59 -0500

    Created HousingUnit model

---
+++ AptManager/Models/HousingUnit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AptManager.Models
+{
+    public class HousingUnit
+    {
+        [Key]
+        public int UnitId { get; set; }
+
+        [ForeignKey("Tenant")]
+        [Display(Name = "")]
+        public int TenantId { get; set; }
+        public Tenant Tenant { get; set; }
+
+        [Display(Name = "Monthly rent")]
+        public int MonthlyRent { get; set; }
+
+        [Display(Name = "Number of bedrooms")]
+        public int Bedrooms { get; set; }
+
+        [Display(Name = "Apartment size in square feet")]
+        public int SquareFootage { get; set; }
+
+        [Display(Name = "Outdoor Access")]
+        public bool OutdoorAccess { get; set; }
+
+
+
+    }
+}
