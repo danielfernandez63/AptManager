6202baea15158345fb16ccd37dbdc1b7899b3796
phodgson558 <pghodgson558@gmail.com>
2018-08-06 10:55:49 -0500

    Created Manager model

--- AptManager/Models/Manager.cs
+++ AptManager/Models/Manager.cs
@@ -7,5 +7,19 @@ namespace AptManager.Models
 {
     public class Manager
     {
+        [Key]
+        public int ManagerId { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
     }
 }
