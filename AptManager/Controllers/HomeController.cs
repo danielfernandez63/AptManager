36a2cd4e3065d336c9a5390e62cb98102060f681
phodgson558 <pghodgson558@gmail.com>
2018-08-08 16:05:09 -0500

    Commiting to receive team changes

--- AptManager/Controllers/HomeController.cs
+++ AptManager/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 
 namespace AptManager.Controllers
 {
@@ -18,6 +20,26 @@ namespace AptManager.Controllers
             return View();
         }
 
+        public ActionResult TwilioTesting()
+        {
+            return View();
+        }
+
+        public ActionResult TwilioMessage()
+        {
+            //TEST CREDENTIALS
+            const string accountSid = "AC2e158f49c90fb7b1447425bc7e2707f1";
+            const string authToken = "2a297079bf5f2b20bd93d0849396c99e";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+                from: new Twilio.Types.PhoneNumber("+15005550006"),
+                to: new Twilio.Types.PhoneNumber("+8473877981")
+                );
+                return RedirectToAction("TwilioTesting");
+        }
 
         public ActionResult About()
         {
