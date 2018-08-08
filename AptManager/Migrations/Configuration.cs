a3b5d273b54cc9b24d115a0a4f5ab997502c3c77
danielfernandez63 <danmanfernan@gmail.com>
2018-08-08 16:56:23 -0500

    added more to config file for Logan to pull and update  his new DB post errors

--- AptManager/Migrations/Configuration.cs
+++ AptManager/Migrations/Configuration.cs
@@ -20,13 +20,13 @@ namespace AptManager.Migrations
             new Models.HousingUnit() { UnitId = 1, Bedrooms = 2, MonthlyRent = 1775, OutdoorAccess = false, SquareFootage = 1050 });
 
             context.Managers.AddOrUpdate(mng => mng.ManagerId,
-                new Models.Manager() { ManagerId = 1, FirstName = "Lisa", LastName = "Bock", Email = "Bock@gmail.com", PhoneNumber = "6086035693" });
+                new Models.Manager() { ManagerId = 1, FirstName = "Lisa", LastName = "Bock", Email = "Bock@gmail.com", PhoneNumber = "6086035693" , ApplicationUserId = "11778b5f-f710-4d3c-aca0-b6ca2b441152" });
 
             context.Workers.AddOrUpdate(wrk => wrk.WorkerId,
-                new Models.Worker() { WorkerId = 1, FirstName = "Peter", LastName = "Griffin", Email = "Peter@gmail.com", PhoneNumber = "6905893697" });
+                new Models.Worker() { WorkerId = 1, FirstName = "Peter", LastName = "Griffin", Email = "Peter@gmail.com", PhoneNumber = "6905893697", ApplicationUserId = "ebd36e7b-85e4-4359-96ee-d76967f845bf"  });
 
             context.Tenants.AddOrUpdate(tnt => tnt.TenantId,
-            new Models.Tenant() { TenantId = 1, FirstName = "Paxton", LastName = " Logan", BalanceDue = 0, Email = "Paxton@gmail.com", PhoneNumber = "6086908875", UnitId = 1 });
+            new Models.Tenant() { TenantId = 1, FirstName = "Paxton", LastName = " Logan", BalanceDue = 0, Email = "Paxton@gmail.com", PhoneNumber = "6086908875", UnitId = 1, ApplicationUserId = "03da613e-5130-4c23-bea6-822fc636fd57" });
 
 
             context.MaintenanceOrders.AddOrUpdate(ord => ord.OrderId,
