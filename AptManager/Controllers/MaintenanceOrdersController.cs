797572b41be84485f8a2670d3877a51f55e7ed69
phodgson558 <pghodgson558@gmail.com>
2018-08-07 19:23:21 -0500

    commiting to receive team changes

--- AptManager/Controllers/MaintenanceOrdersController.cs
+++ AptManager/Controllers/MaintenanceOrdersController.cs
@@ -28,6 +28,8 @@ namespace AptManager.Controllers
             if (User.Identity.IsAuthenticated && User.IsInRole("Maintenance" ))
             {
                 var id = User.Identity.GetUserId();
+
+
                 var worker = db.Workers.Where(w => w.WorkerId.Equals(id)).First();
                 var maintenanceOrders = db.MaintenanceOrders.Include(m => m.WorkerId == worker.WorkerId && m.IsCompleted != true).ToList();
                 return View(maintenanceOrders.ToList());
